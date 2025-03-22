014e3e536274acd98d864a06eee9bb3ea77abfd8
thiagozaneti <dotinha0203@gmail.com>
2025-03-22 12:49:03 -0300

    programa finalizado, funcionando e documentado

--- logic.py
+++ logic.py
@@ -1,37 +1,45 @@
 from menu import initial_menu, circle_menu, rectangle_menu, square_menu, triangle_menu
+##importação de funções que exibem os menus do arquivo menu.py
 
 ##nessa lógica, usarei um while e match case 
 
-PI =  3.14159
+PI =  3.14159 ##constante PI
+
 
 def logic():
     while True:
         initial_menu() ##menu inicial
-        initial_user_select = int(input("Digite sua seleção")) ## seleção do usuario
+        initial_user_select = int(input("Digite sua seleção")) ## seleção inicial do usuário
         match initial_user_select:
             case 0:
-                print("Encerrando o programa")
+                print("Encerrando o programa") 
                 break
+            ##caso o usuario digite 0, o programa encerra
             case 1:
                 triangle_menu()
                 triangle_user_base = float(input("Digite o valor da base:"))
                 triangle_user_height = float(input("Digite o valor da altura:"))
                 print(f"O valor da área do triangulo é: {(triangle_user_base*triangle_user_height)/2}")
+            ##caso o usuário digite 1, calcula a área do tringulo
             case 2: 
                 rectangle_menu()
                 rectangle_user_base = float(input("Digite o valor da base:"))
                 rectangle_user_height = float(input("Digite o valor da altura: "))
                 print(f"O valor da área do retangulo é:{rectangle_user_base*rectangle_user_height}")
+            ##caso o usuário digite 2, calcula a área do retangulo 
             case 3:
                 square_menu()
                 square_user_side = float(input("Digite o valor do lado do quadrado: "))
                 print(f"O valor da área do quadrado é: {square_user_side*square_user_side}")
+            ##caso o usuário digite 3, calcular a área do quadrado 
             case 4: 
                 circle_menu()
                 circle_user_raio = float(input("Digite o valor do raio do circulo: "))
                 print(f"O valor da área do raio é {PI*(circle_user_raio**2)}")
+            ##caso o usuário digite 4, calcula a área do circulo
             case _:
                 print("Valor inválido")
                 break
+            ##caso o usuário digite algo que não possa digitar, o programa quebra
         
 
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from logic import logic
 
+
+##execução da função logic() que carrega a logica completa do programa
 if __name__ == "__main__":
     logic()
